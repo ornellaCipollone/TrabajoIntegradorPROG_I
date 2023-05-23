94202f2328572cb4a944f33f843ccc11ca300a88
ramiro2805 <rlohrmann@udesa.edu.ar>
2023-05-23 18:17:24 -0300

    arregle el buscador y agregue diseno

---
+++ js/genres.js
@@ -0,0 +1,17 @@
+let buscador = document.querySelector("#buscador")
+let formulario = document.querySelector("#form")
+
+formulario.addEventListener('submit', function(e) {;
+ let terminoBuscador = buscador.value.trim();
+
+if (terminoBuscador === "") {
+    e.preventDefault()
+    alert("ingrese texto")
+    return
+}
+if (terminoBuscador.length < 3) {
+    e.preventDefault()
+    alert("debe tener mas de 3 caracteres")
+    return
+}
+});
