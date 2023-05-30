b4a60e153382106c7f623f8997b9edc6be1132b6
ramiro2805 <rlohrmann@udesa.edu.ar>
2023-05-30 16:52:35 -0300

    mas avances del index

--- js/index.js
+++ js/index.js
@@ -24,10 +24,40 @@ fetch ("https://api.allorigins.win/raw?url="+"https://api.deezer.com/chart/0/alb
         console.log(data);
         let contenedor = document.querySelector(".albumes")
         for (let i=0 ; i<6; i++ )
-        contenedor.innerHTML += `<li> <a href="./detail-album.html"><img src=${data.data[i].cover}><h4>${data.data[i].title}</h4></a></li>`
+        contenedor.innerHTML += `<li class="listados"> <a href="./detail-album.html"><img src=${data.data[i].cover}><h4 class="tituloAlbum">${data.data[i].title}</h4></a></li>`
         
 
     })
     .catch (function(error){
         console.log(error)
     })
+
+    fetch("https://api.allorigins.win/raw?url="+"https://api.deezer.com/chart/0/artists")
+        .then(function(response){
+        return response.json()
+        })
+        .then(function(data){   
+         console.log(data);
+         let contenedorArtista = document.querySelector(".artistas")
+         for (let i=0 ; i<6; i++ )
+        contenedorArtista.innerHTML += `<li class="listados"> <a href="./detail-artist.html"><img src=${data.data[i].picture}><h4 class="nombreArtista">${data.data[i].name}</h4></a></li>`
+        })
+        .catch (function(error){
+            console.log(error)
+        })
+
+        fetch("https://api.allorigins.win/raw?url="+"https://api.deezer.com/chart")
+        .then(function(response){
+        return response.json()
+        })
+        .then(function(data){   
+         console.log(data)
+         let contenedorTrack = document.querySelector(".canciones")
+         for (let i=0 ; i<6; i++ )
+        contenedorTrack.innerHTML += `<li class="listados"> <a href="./detail-track.html"><img src=${data.tracks.data[i].album.cover}></a><h4 class="nombreTrack" ><a href="./detail-artist"></a>${data.tracks.data[i].title}</h4>
+        <h5 class="nombreArtista">${data.tracks.data[i].artist.name}</h5>
+        </li>`
+        })
+        .catch (function(error){
+            console.log(error)
+        })
