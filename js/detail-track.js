b9a36febd655c1e598eec79778430c4abd669ee6
ramiro2805 <rlohrmann@udesa.edu.ar>
2023-06-09 11:47:14 -0300

    avanzes playlist

--- js/detail-track.js
+++ js/detail-track.js
@@ -2,7 +2,23 @@ let queryString = location.search;
 let queryObj = new URLSearchParams(queryString)
 let id = queryObj.get("id") 
 console.log(id)
+let buscador = document.querySelector("#buscador")
+let formulario = document.querySelector("#contenedor-form")
 
+formulario.addEventListener('submit', function(e) {;
+    let terminoBuscador = buscador.value.trim();
+
+if (terminoBuscador === "") {
+    e.preventDefault()
+    alert("ingrese texto")
+    return
+}
+if (terminoBuscador.length < 3) {
+    e.preventDefault()
+    alert("debe tener mas de 3 caracteres")
+    return
+}
+});
 
 fetch("https://api.allorigins.win/raw?url=https://api.deezer.com/track/" + id)
 .then(function(response){
@@ -21,10 +37,40 @@ fetch("https://api.allorigins.win/raw?url=https://api.deezer.com/track/" + id)
 
     <article>
     <h1>Agregar a mi playlist</h1>
-    <h2>Mi playlist</h2>
+    <a href="playlist.html"><h2>Mi playlist</h2></a>
     </article>  
     `
 })
 .catch(function(error){
     console.log("el error es:" + error)
 })
+let favoritos=[]
+
+let recupero = localStorage.getItem('favoritos')
+let favs = JSON.parse(recupero)
+for (i=0; i < favs.length; i++){
+    favoritos.push(favs[i])
+}
+
+
+let botonFav= document.querySelector(".fav")
+botonFav.addEventListener('click',function(e){
+    if (favoritos.includes(id)){
+        let indice = favoritos.indexOf(id)
+        console.log(indice)
+        favoritos.splice(indice,1)
+        botonFav.innerText = "Agregar a playlist"
+    }
+    else {
+        botonFav.innerText="Quitar de playlist"
+        favoritos.push(id)
+    }
+    let stringify = JSON.stringify(favoritos)
+    localStorage.setItem('favoritos',favoritos)
+    console.log(favoritos)
+})
+
+
+
+console.log(favoritos)
+
--- js/playlist.js
+++ js/playlist.js
@@ -1,5 +1,5 @@
 let buscador = document.querySelector("#buscador")
-let formulario = document.querySelector("#form")
+let formulario = document.querySelector("#contenedor-form")
 
 formulario.addEventListener('submit', function(e) {;
  let terminoBuscador = buscador.value.trim();
@@ -15,3 +15,21 @@ if (terminoBuscador.length < 3) {
     return
 }
 });
+
+let recupero = localStorage.getItem('favoritos')
+let favoritos = JSON.parse(recupero)
+console.log(favoritos)
+
+for (i=0; i <= favoritos.length; i++) {
+    fetch("https://api.allorigins.win/raw?url=https://api.deezer.com/track/?id=" + favoritos[i])
+    .then(function(repsponse){
+        return repsponse.json
+    })
+    .then(function(data){
+        console.log(data)
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+}
+
